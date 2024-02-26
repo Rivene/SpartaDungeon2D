257accd9bac4ef0827396e481118471ede65d360
dch1114 <dch1114@naver.com>
2024-02-26 20:01:49 +0900

    [Fix]240226_speed 수정

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -15,5 +15,6 @@ public class CharacterStats
 {
     public StatsChangeType statsChangeType;
     [Range(1, 100)] public int maxHealth;
+    [Range(1f, 20f)] public float speed;
     public AttackSO attackSO;
 }
